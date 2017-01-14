76c91d9669684efbaed003912ebefa30e6a4c33d
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-14 11:26:09 +0000

    WIP

--- src/python/datapreprocessor/datapreprocessor/metadata.py
+++ src/python/datapreprocessor/datapreprocessor/metadata.py
@@ -1,3 +1,12 @@
+import csv
+import os
+
+from csvcleanser import csvcleanser
+from csvreader import csvreader
+from datanormaliser import datanormaliser
+
+
+DEFAULT_BASE_PATH = '../../../php/gov-harvester/datafiles'
 METADATA_COLUMN_KEYS = [
     'type',
     'title',
@@ -7,5 +16,47 @@ METADATA_COLUMN_KEYS = [
     'source_url',
 ]
 
+
+def process_metadata_file(filename, error_collection):
+    for row in csvreader.read_file(filename):
+        metadata_dict = convert_metadata_row_to_dict(row)
+        process_file_from_metadata(metadata_dict, error_collection)
+
+
 def convert_metadata_row_to_dict(metadata_row):
     return dict(zip(METADATA_COLUMN_KEYS, metadata_row))
+
+
+def process_file_from_metadata(
+    metadata_dict, error_collection, base_path=DEFAULT_BASE_PATH
+):
+    filename = "_".join(metadata_dict['source_url'].split('/')[-2:])
+    for row in process_csv_file(filename, error_collection):
+        print row
+
+
+def process_csv_file(filename, error_collection):
+    file_info = datanormaliser.extract_info_from_filename(filename)
+    current_minister = None
+    new_rows = []
+    row_index = 0
+    for row in csvreader.read_file(filename):
+        try:
+            row_index += 1
+            clean_row_data = csvcleanser.cleanse_row(row)
+            if clean_row_data is None:
+                # row didn't contain useful data.
+                continue
+            new_row, current_minister = datanormaliser.normalise_row(
+                clean_row_data, file_info['year'], current_minister
+            )
+            new_row.append(metadata_dict['office'])
+            yield new_row
+        except Exception, e:
+            error_collection.append({
+                'filename': filename,
+                'row_index': row_index,
+                'row_data': row,
+                'exception': repr(e),
+                'type': 'row_error',
+            })
--- src/python/datapreprocessor/test/metadata_tests.py
+++ src/python/datapreprocessor/test/metadata_tests.py
@@ -1,7 +1,10 @@
+from mock import mock_open, patch, sentinel
 import unittest
 from datapreprocessor.metadata import (
-    convert_metadata_row_to_dict
+    convert_metadata_row_to_dict,
+    process_file_from_metadata,
 )
+
 
 class ConvertMetadataRowToDictTestCase(unittest.TestCase):
 
@@ -21,3 +24,25 @@ class ConvertMetadataRowToDictTestCase(unittest.TestCase):
         metadata_dict = convert_metadata_row_to_dict(metadata_row)
 
         self.assertEqual(expected_output, metadata_dict)
+
+
+class ProcessFileFromMetadataTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.sample_data = {
+            'type': 'Col1',
+            'title': 'Col2',
+            'office': 'Col3',
+            'collection': 'Col4',
+            'date_published': 'Col5',
+            'source_url': 'http://base_url.com/some/url/file_number/filename.ext',
+        }
+
+    @patch('datapreprocessor.metadata.csv.reader')
+    @patch('datapreprocessor.metadata.open', new_callable=mock_open())
+    def test_opens_correct_file_based_on_url(self, mock_open_func, mock_reader):
+        mock_reader.return_value = []
+
+        process_file_from_metadata(self.sample_data, base_path='../a/path')
+
+        mock_open_func.assert_called_once_with('../a/path/file_number_filename.ext')
